96f8a871d631181728732da14000e88a4d91201b
Weslley Victor <wvictor07@gmail.com>
2021-01-14 16:47:43 +0000

    first commit on new branch dealing with new format. Also added pdf reports to repo.

--- tests/test_global.py
+++ tests/test_global.py
@@ -5,7 +5,8 @@ import sample.format as format
 import sample.report as report
 
 def test_PDF_format():
-    latest_report_date = report.info_latest()['report_date'].replace('/','-')
+    #latest_report_date = report.info_latest()['report_date'].replace('/','-')
+    latest_report_date = '20-12-2020'
     filename = 'var/'+latest_report_date+'.pdf'
     #downloading DGS report
     report.download(report.info_latest()['link'], filename)
@@ -15,6 +16,6 @@ def test_PDF_format():
     age_and_gender = report.get_data_by_age_and_gender(filename)
     print(data_summary)
     #deleting report
-    report.delete(filename)
+    #report.delete(filename)
     #assertion
     assert format.are_values_valid(data_summary) and format.are_values_valid(data_hospt) and format.are_values_valid(age_and_gender) and len(data_summary) == 9 and len(data_hospt) == 2 and len(age_and_gender) == 4
