0f13e42362025eb8473a77b2291cec463f8c4928
Weslley Victor <wvictor07@gmail.com>
2021-04-03 17:23:34 +0100

    updated get_li_items() method to account for change in DGS website format and updated .csv file with most current data up to this point

--- sample/website.py
+++ sample/website.py
@@ -4,7 +4,10 @@ import requests
 def get_li_items():
     page = requests.get('https://covid19.min-saude.pt/relatorio-de-situacao/')
     soup = BeautifulSoup(page.content, 'html.parser')
-    div = soup.find("div", class_="c-accordion__content")
-    ul = div.find_all("ul")[0]
-    li_tags = ul.find_all('li')#li with all links for reports
+    div = soup.find_all("div", class_="c-accordion__content")
+    li_tags = []
+    for tag in div:
+        lis = tag.find_all('ul')[0].find_all('li')
+        for li in lis:
+            li_tags.append(li)
     return li_tags
