152642414bea2ca3b697355b94e7cea5b74d8288
Weslley Victor <wvictor07@gmail.com>
2021-04-12 11:06:55 +0100

    Implemented test for incidence and r(t) data

--- tests/test_global.py
+++ tests/test_global.py
@@ -15,12 +15,17 @@ def test_PDF_format():
     data_summary = report.get_summary_data(filename)
     data_hospt = report.get_hospitalized_data(filename)
     age_and_gender = report.get_data_by_age_and_gender(filename)
+    coefficients = report.get_incidence_and_transmissibility(filename)
     print(data_summary,data_hospt)
 
     #deleting report
     report.delete(filename)
     #assertion
     assert format.are_values_valid(data_summary) and format.are_values_valid(data_hospt) and format.are_values_valid(age_and_gender) and len(data_summary) == 9 and len(data_hospt) == 2 and len(age_and_gender) == 4
+
+    #assertions for incidence and r(t) method
+    for col in coefficients.keys():
+        assert type(coefficients[col]) is float
 
 def test_get_recent_data_age_gender():
     req = urllib.request.Request('https://covid19-api.vost.pt/Requests/get_last_update')
